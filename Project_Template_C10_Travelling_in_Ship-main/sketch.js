4496787aa478af54453785105b4265db978cf122
shivanipt <87143211+shivanipt@users.noreply.github.com>
2021-07-10 12:29:03 -0500

    7/10 Resubmission

--- Project_Template_C10_Travelling_in_Ship-main/sketch.js
+++ Project_Template_C10_Travelling_in_Ship-main/sketch.js
@@ -2,21 +2,21 @@ var ship_swimming, ship;
 var seaImage, sea;
 
 function preload(){
-  ship_swimming = loadAnimation("ship-1/png", "ship-2.png", "ship-3.png", "ship-4.png");
+  ship_swimming = loadAnimation("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
   seaImage = loadImage("sea.png");
 }
 
 function setup(){
   createCanvas(400,400);
 
-  ship = createSprite(50, 160, 20, 50);
-  ship.addAnimation("swimming", ship_swimming);
-  ship.scale = 0.75;
-  edges = createEdgeSprites();
-
-  sea = createSprite(400, 400);
+  sea = createSprite(10, 10);
   sea.addImage("sea", seaImage);
   sea.x = sea.width/2;
+
+  ship = createSprite(125, 200, 20, 50);
+  ship.addAnimation("swimming", ship_swimming);
+  ship.scale = 0.35;
+  edges = createEdgeSprites();
 
 }
 
